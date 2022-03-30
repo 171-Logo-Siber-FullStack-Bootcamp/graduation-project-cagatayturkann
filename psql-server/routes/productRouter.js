08f17d58637aec0fbb26ab5e8959df435ce5dc73
Çağatay Türkan <cagatayturkann@gmail.com>
2022-03-30 13:48:03 +0300

    psql server added

---
+++ psql-server/routes/productRouter.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const productController = require('../controllers/productController');
+const multer = require('multer');
+
+const router = express.Router();
+
+//multer middleware
+let storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, './uploads');
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname);
+	},
+});
+
+let upload = multer({
+	storage: storage,
+}).single('image');
+
+router.get('/', productController.getAllProduct);
+router.get('/search', productController.getAllProductFromElastic);
+router.get('/:id', productController.getProduct);
+router.post('/', upload, productController.addProduct);
+router.put('/:id', upload, productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
+router.delete('/', productController.deleteAllProduct);
+
+module.exports = router;
